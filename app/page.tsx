b8794db71f9ed29d2b490fd753b7bab53ec6c7a1
tornike2002 <toko.toka2000@gmail.com>
2024-07-22 18:42:18 +0400

    basic design of mobile navbar is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return <Navbar />;
+  return (
+    <main className="bg-black">
+      <Navbar />;
+    </main>
+  );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,17 @@
-
-
+import Image from "next/image";
+import logo from "../public/assets/shared/desktop/logo.svg";
+import Cart from "../public/assets/shared/desktop/icon-cart.svg";
+import Hamburger from "../public/assets/shared/tablet/icon-hamburger.svg";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <nav className="flex justify-between items-center px-6 py-8">
+        <Image src={Hamburger} alt="hamburgermenu" className="cursor-pointer" />
+        <Image src={logo} alt="Logo" className="cursor-pointer" />
+        <Image src={Cart} alt="Cart" className="cursor-pointer" />
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
