8546801bead1077173f4cd2cbd208e340f9c2b42
tornike2002 <toko.toka2000@gmail.com>
2024-07-26 19:10:48 +0400

    finished another section on home page but mobile

--- app/page.tsx
+++ app/page.tsx
@@ -88,12 +88,19 @@ export default function Home() {
         </div>
       </section>
       {/* info section */}
-      <section className="mx-6 flex flex-col">
+      <section className="mx-6 flex flex-col gap-3 mb-24">
         <Image src={info} alt="guy with headphones" />
-        <h1 className="text-center">
-          Bringing you the <span className="">best</span> audio gear
+        <h1
+          className="text-center text-black font-Manrope text-[28px] 
+        font-bold tracking-[1px] uppercase"
+        >
+          Bringing you the
+          <br /> <span className="text-customYellow">best</span> audio gear
         </h1>
-        <p className="text-center">
+        <p
+          className="text-center text-black font-Manrope 
+        text-[15[x] leading-[25px] opacity-50"
+        >
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
