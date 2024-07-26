093e74c3d07fb0d5b39d8d3515ecd7f5b8bf0c28
tornike2002 <toko.toka2000@gmail.com>
2024-07-26 19:01:24 +0400

    Added info section to Home page and custom yellow color to Tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Cardspeaker from "../public/assets/home/mobile/image-speaker-zx9.png";
 import YellowBanner from "@/components/YellowBanner";
 import greySpeaker from "../public/assets/home/mobile/image-speaker-zx7.jpg";
 import blackairpods from "../public/assets/home/mobile/image-earphones-yx1.jpg";
+import info from "../public/assets/shared/mobile/image-best-gear.jpg";
 import Image from "next/image";
 export default function Home() {
   return (
@@ -85,6 +86,21 @@ export default function Home() {
             SEE PRODUCT
           </button>
         </div>
+      </section>
+      {/* info section */}
+      <section className="mx-6 flex flex-col">
+        <Image src={info} alt="guy with headphones" />
+        <h1 className="text-center">
+          Bringing you the <span className="">best</span> audio gear
+        </h1>
+        <p className="text-center">
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
       </section>
     </main>
   );
