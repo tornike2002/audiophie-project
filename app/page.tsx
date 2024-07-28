144b85d3a60e3b10b4cd05ab63e103ba222c5784
tornike2002 <toko.toka2000@gmail.com>
2024-07-28 16:27:39 +0400

    made blog reusable

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Cardspeaker from "../public/assets/home/mobile/image-speaker-zx9.png";
 import YellowBanner from "@/components/YellowBanner";
 import greySpeaker from "../public/assets/home/mobile/image-speaker-zx7.jpg";
 import blackairpods from "../public/assets/home/mobile/image-earphones-yx1.jpg";
-import info from "../public/assets/shared/mobile/image-best-gear.jpg";
 import Image from "next/image";
 export default function Home() {
   return (
@@ -88,27 +87,7 @@ export default function Home() {
         </div>
       </section>
       {/* info section */}
-      <section className="mx-6 flex flex-col gap-3 mb-24">
-        <Image src={info} alt="guy with headphones" />
-        <h1
-          className="text-center text-black font-Manrope text-[28px] 
-        font-bold tracking-[1px] uppercase"
-        >
-          Bringing you the
-          <br /> <span className="text-customYellow">best</span> audio gear
-        </h1>
-        <p
-          className="text-center text-black font-Manrope 
-        text-[15[x] leading-[25px] opacity-50"
-        >
-          Located at the heart of New York City, Audiophile is the premier store
-          for high end headphones, earphones, speakers, and audio accessories.
-          We have a large showroom and luxury demonstration rooms available for
-          you to browse and experience a wide range of our products. Stop by our
-          store to meet some of the fantastic people who make Audiophile the
-          best place to buy your portable audio equipment.
-        </p>
-      </section>
+   
     </main>
   );
 }
