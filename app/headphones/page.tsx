b6352c904c9933e099470f747e7fcce0f8b348cd
tornike2002 <toko.toka2000@gmail.com>
2024-07-28 18:28:45 +0400

    added another product xx59headphones

--- app/headphones/page.tsx
+++ app/headphones/page.tsx
@@ -2,6 +2,7 @@ import CustomHeaderBanner from "@/components/CustomHeaderBanner";
 import ProductDisplay from "@/components/ProductDisplay";
 import headphonex99two from "../../public/assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg";
 import headphonex99one from "../../public/assets/product-xx99-mark-one-headphones/mobile/image-category-page-preview.jpg";
+import headphonex59 from "../../public/assets/product-xx59-headphones/mobile/image-category-page-preview.jpg";
 const Headphones = () => {
   return (
     <>
@@ -17,6 +18,12 @@ const Headphones = () => {
         isNew=""
         title={`XX99 Mark I Headphones`}
         about="As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go."
+      />
+      <ProductDisplay
+        image={headphonex59}
+        isNew=""
+        title={`XX59 Headphones`}
+        about="Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move."
       />
     </>
   );
