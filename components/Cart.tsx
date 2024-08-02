825bb942cdb959ad270bdd4ac739715702e80269
tornike2002 <toko.toka2000@gmail.com>
2024-08-02 23:25:51 +0400

    added newcart with functionality

---
+++ components/Cart.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useState } from "react";
+import { Button, Modal } from "antd";
+
+const Cart = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+};
+
+export default Cart;
