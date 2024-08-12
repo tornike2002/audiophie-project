536ec93781c8c82577f097d5ce69c696bb9a38cb
tornike2002 <toko.toka2000@gmail.com>
2024-08-12 20:05:24 +0400

    added detiles and summary section

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -12,23 +12,10 @@ const layout = {
 const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
+
 const CheckoutPage = () => {
   const [form] = Form.useForm();
 
-  const onGenderChange = (value: string) => {
-    switch (value) {
-      case "male":
-        form.setFieldsValue({ note: "Hi, man!" });
-        break;
-      case "female":
-        form.setFieldsValue({ note: "Hi, lady!" });
-        break;
-      case "other":
-        form.setFieldsValue({ note: "Hi there!" });
-        break;
-      default:
-    }
-  };
   const onFinish = (values: any) => {
     console.log(values);
   };
@@ -38,47 +25,85 @@ const CheckoutPage = () => {
   };
 
   return (
-    <section>
-      <Link href="/">Go Back</Link>
+    <section className="px-6 py-5">
+      <Link
+        href="/"
+        className="text-black opacity-50 font-Manrope text-[15px] leading-[25px] my-5"
+      >
+        Go Back
+      </Link>
       <div>
         <Form
           {...layout}
           form={form}
           name="control-hooks"
           onFinish={onFinish}
-          style={{ maxWidth: 600 }}
+          style={{ maxWidth: 600, paddingTop: "10px" }}
         >
-          <Form.Item name="note" label="Note" rules={[{ required: true }]}>
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: "Please fill the name field!" }]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-            <Select
-              placeholder="Select Gender"
-              onChange={onGenderChange}
-              allowClear
-            >
-              <Option value="male">male</Option>
-              <Option value="female">female</Option>
-              <Option value="other">other</Option>
-            </Select>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              {
+                required: true,
+                type: "email",
+                message: "Valid email is required",
+              },
+            ]}
+          >
+            <Input />
           </Form.Item>
           <Form.Item
-            noStyle
-            shouldUpdate={(prevValues, currentValues) =>
-              prevValues.gender !== currentValues.gender
-            }
+            name="phone"
+            label="Phone"
+            rules={[{ required: true, message: "Phone number is required" }]}
           >
-            {({ getFieldValue }) =>
-              getFieldValue("gender") === "other" ? (
-                <Form.Item
-                  name="customizeGender"
-                  label="Customize Gender"
-                  rules={[{ required: true }]}
-                >
-                  <Input />
-                </Form.Item>
-              ) : null
-            }
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="address"
+            label="Address"
+            rules={[{ required: true, message: "Address is required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="zipCode"
+            label="Zip Code"
+            rules={[{ required: true, message: "Zip code is required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="city"
+            label="City"
+            rules={[{ required: true, message: "City is required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="country"
+            label="Country"
+            rules={[{ required: true, message: "Country is required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="gender"
+            label="Gender"
+            rules={[{ required: true, message: "Gender is required" }]}
+          >
+            <Select placeholder="Select Gender" allowClear>
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+            </Select>
           </Form.Item>
           <Form.Item {...tailLayout}>
             <Space>
@@ -91,6 +116,37 @@ const CheckoutPage = () => {
             </Space>
           </Form.Item>
         </Form>
+      </div>
+      <div className="flex justify-start sm:justify-center max-w-[600px]">
+        <div className="flex flex-col gap-2 pb-20 pt-10 w-[300px]">
+          <h1 className="text-black font-Manrope text-lg font-bold tracking-[1.286px] uppercase">
+            SUMMARY
+          </h1>
+          <div className="flex items-center justify-between">
+            <span className="text-black font-Manrope text-[15px] leading-[25px] opacity-50">
+              TOTAL
+            </span>
+            <span className="text-black font-Manrope text-lg font-bold uppercase">
+              #Price
+            </span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-black font-Manrope text-[15px] leading-[25px] opacity-50">
+              SHIPPING
+            </span>
+            <span className="text-black font-Manrope text-lg font-bold uppercase">
+              $50
+            </span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-black font-Manrope text-[15px] leading-[25px] opacity-50">
+              GrandTotal:
+            </span>
+            <span className="text-customYellow font-Manrope text-lg font-bold uppercase">
+              $Price
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
